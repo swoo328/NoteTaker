f6384d450cbb83d35280becfd089b48769b525a9
Steven Woo <swoo9875@gmail.com>
2021-03-06 16:36:19 -0500

    created app.post to create and store the data in the database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,27 @@
 const db = require("../db/db.json");
 const path = require("path");
 const fs = require("fs");
-  
-  module.exports = function (app) {
-    //
-    app.get("./api/notes", function(req,res){
-      res.sendFile(path.join(__dirname, "./db/db.json"));
-    });
-    
-    app.post("./api/notes", function(req,res){
 
-    });
-  };
+module.exports = function (app) {
+  //gets the notes that are being created 
+   app.get("/api/notes", function (req, res) {
+    res.json(db);
+    console.log(db);
+  });
+  //Creates the notes and saving it to the database
+  app.post("/api/notes", function (req, res) {
+    let newNote={
+       title: req.body.title,
+       text: req.body.text,
+       //length of the data in the database
+       id: db.length
+    };
+    //pushes the Note to the database
+    db.push(newNote);
+    console.log(newNote); 
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(db), err => {
+      if (err) { console.log(err) }
+      res.json(newNote);
+      }); 
+   });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,4 +10,5 @@ module.exports = function (app) {
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
+
 }   
